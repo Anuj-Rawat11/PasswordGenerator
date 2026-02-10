6abae3fca158395cf9f4b24d354052c52b7cc49a
Anuj-Rawat11 <8909rawatanuj@gmail.com>
2026-02-10 23:28:32 +0530

    Set states for range and two checkboxes

--- password-generator/src/App.js
+++ password-generator/src/App.js
@@ -1,16 +1,21 @@
 // import logo from './logo.svg';
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+const [rangevalue,setrangevalue]=useState(10);
+const [numcheck,setnumcheck]=useState(false);
+const [charcheck,setcharcheck]=useState(false);
   return (
     <div className="App">
-      <div class="passapp">
+      <div className="passapp">
         <h1>Password:</h1>
         <label>Select Length : </label>
-        <input type="range" min="5" max="30" />
-        <input id="num" type="checkbox" />
+        <input type="range" min="5" max="30" value={rangevalue} onChange={(e)=>setrangevalue(e.target.value)} />
+        <span>{rangevalue}</span>
+        <input id="num" type="checkbox" defaultValue={numcheck} onChange={(e)=>setnumcheck(e.target.value)}/>
         <label htmlFor="num">Number</label>
-        <input  id="char" type="checkbox" />
+        <input  id="char" type="checkbox" defaultValue={charcheck} onChange={(e)=>setcharcheck(e.target.value)} />
         <label htmlFor="char">Special characters</label>
       </div>
     </div>
