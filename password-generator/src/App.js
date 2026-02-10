359673566c8707d75301228505ce5741437622a0
Anuj-Rawat11 <8909rawatanuj@gmail.com>
2026-02-10 23:17:16 +0530

    Designed basic design of the application

--- password-generator/src/App.js
+++ password-generator/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="passapp">
+        <h1>Password:</h1>
+        <label>Select Length : </label>
+        <input type="range" min="5" max="30" />
+        <input id="num" type="checkbox" />
+        <label htmlFor="num">Number</label>
+        <input  id="char" type="checkbox" />
+        <label htmlFor="char">Special characters</label>
+      </div>
     </div>
   );
 }
